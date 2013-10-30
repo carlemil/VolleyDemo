a8cae3a89fde031fc876203351986a4485d98c25
erbsman <erbsman@gmail.com>
2013-10-30 19:15:21 +0100

    moved the figuring out how much mem we got to the application class, and fixed the maths so that it acctualy returns something usefull.

--- src/se/kjellstrand/volleydemo/VolleyDemoApplication.java
+++ src/se/kjellstrand/volleydemo/VolleyDemoApplication.java
@@ -3,11 +3,14 @@ package se.kjellstrand.volleydemo;
 import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
 /**
  * Created by erbsman on 7/25/13.
  */
 public class VolleyDemoApplication extends Application {
+
+    private static final String TAG = VolleyDemoApplication.class.getCanonicalName();
 
     private static VolleyDemoApplication sInstance;
 
@@ -20,8 +23,12 @@ public class VolleyDemoApplication extends Application {
         super.onCreate();
         sInstance = this;
 
-        int cacheSize = ((ActivityManager) getSystemService(Context.ACTIVITY_SERVICE))
-                .getMemoryClass() / 8;
+        int memoryClass = ((ActivityManager) getSystemService(Context.ACTIVITY_SERVICE))
+                .getMemoryClass();
+        // move from Mb to Kb, and use about 1/4th of the availiable memmory for
+        // cacheing images.
+        int cacheSize = memoryClass * 1024 / 4;
+        Log.d(TAG, "Setting cache size to: " + cacheSize + " bytes.");
         VolleySingleton.initialize(getApplicationContext(), cacheSize);
 
     }
